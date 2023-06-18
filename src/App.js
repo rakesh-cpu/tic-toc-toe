c0db1a69f3558311c07e69ab2fdb301eea929bb2
rakesh-cpu <rakeshdontula66@gmail.com>
2023-06-18 14:47:39 +0530

    setup gh-ages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tic-toc-Toe app
         </a>
       </header>
     </div>
